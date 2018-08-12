760031b4ce4df2a52de2f39654ab9f2c6982bc0a
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2018-08-12 16:23:36 -0400

    receive SMS action

--- app/src/main/java/com/pengdaniel/imnapping/MainActivity.kt
+++ app/src/main/java/com/pengdaniel/imnapping/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO("prompt for user permissions")
     }
 }
--- app/src/main/java/com/pengdaniel/imnapping/SmsReceiver.kt
+++ app/src/main/java/com/pengdaniel/imnapping/SmsReceiver.kt
@@ -3,14 +3,19 @@ package com.pengdaniel.imnapping
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.provider.Telephony
+import android.widget.Toast
 
 class SmsReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        if (intent?.action?.equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION) == true) {
+            Toast.makeText(context, "Message received", Toast.LENGTH_LONG).show()
+        }
     }
 
     companion object {
-        private val TAG = "SmsReceiver"
+        private val TAG = SmsReceiver::class.java.name
     }
 }
