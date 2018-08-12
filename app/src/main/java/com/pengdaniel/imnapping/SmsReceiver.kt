c0fdfad13dce1237dcdfb2d2bd7b3a1a8189a113
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2018-08-11 22:52:13 -0400

    add intent filter and permission to manifest, add SmsReceiver

---
+++ app/src/main/java/com/pengdaniel/imnapping/SmsReceiver.kt
@@ -0,0 +1,16 @@
+package com.pengdaniel.imnapping
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SmsReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    companion object {
+        private val TAG = "SmsReceiver"
+    }
+}
