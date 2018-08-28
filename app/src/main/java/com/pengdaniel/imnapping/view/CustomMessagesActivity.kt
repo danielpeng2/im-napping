7bf23a6fa308538e66c6d555edd7e7d14bfeebf8
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2018-08-27 22:47:13 -0400

    add toolbar with up navigation to CustomMessagesActivity

--- app/src/main/java/com/pengdaniel/imnapping/view/CustomMessagesActivity.kt
+++ app/src/main/java/com/pengdaniel/imnapping/view/CustomMessagesActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.pengdaniel.imnapping.R
 import com.pengdaniel.imnapping.presenter.CustomMessagesPresenter
+import kotlinx.android.synthetic.main.activity_custom_messages.*
 
 class CustomMessagesActivity : AppCompatActivity(), CustomMessagesView {
 
@@ -12,6 +13,10 @@ class CustomMessagesActivity : AppCompatActivity(), CustomMessagesView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_messages)
+
+        setSupportActionBar(custom_messages_toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.title = getString(R.string.title_custom_messages)
 
         presenter = CustomMessagesPresenter()
     }
