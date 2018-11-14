ba4a62c60eeeba6c2317aaef9eb2e4ad11689c28
Daniel Peng <30786057+DanielPeng2@users.noreply.github.com>
2018-11-14 00:24:27 -0500

    add custom messages recyclerview and adapter

--- app/src/main/java/com/pengdaniel/imnapping/view/CustomMessagesActivity.kt
+++ app/src/main/java/com/pengdaniel/imnapping/view/CustomMessagesActivity.kt
@@ -2,13 +2,22 @@ package com.pengdaniel.imnapping.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import com.pengdaniel.imnapping.R
+import com.pengdaniel.imnapping.model.CustomMessage
+import com.pengdaniel.imnapping.model.SharedPrefManager
+import com.pengdaniel.imnapping.model.SharedPrefType
 import com.pengdaniel.imnapping.presenter.CustomMessagesPresenter
 import kotlinx.android.synthetic.main.activity_custom_messages.*
 
 class CustomMessagesActivity : AppCompatActivity(), CustomMessagesView {
 
     private lateinit var presenter: CustomMessagesPresenter
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +30,19 @@ class CustomMessagesActivity : AppCompatActivity(), CustomMessagesView {
         fab.setOnClickListener {
         }
 
+        // TODO: initialize in presenter
+        // Temp data
+        val tempData = arrayListOf(CustomMessage("123", "daniel", "xD"),
+                            CustomMessage("456", "joslyn", "bun"))
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = CustomMessagesAdapter(tempData)
+        recyclerView = custom_messages_list.apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+        val sharedPrefManager = SharedPrefManager(this, SharedPrefType.MESSAGES)
         presenter = CustomMessagesPresenter()
     }
 }
---
+++ app/src/main/java/com/pengdaniel/imnapping/view/CustomMessagesAdapter.kt
@@ -0,0 +1,37 @@
+package com.pengdaniel.imnapping.view
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import com.pengdaniel.imnapping.R
+import com.pengdaniel.imnapping.model.CustomMessage
+import kotlinx.android.synthetic.main.item_custom_message.view.*
+
+class CustomMessagesAdapter(private val customMessages: ArrayList<CustomMessage>):
+        RecyclerView.Adapter<CustomMessagesAdapter.ViewHolder>(){
+
+    class ViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        var nameView: TextView = view.name_text
+        var messageView: TextView = view.message_text
+        var overflowButton: ImageButton = view.overflow_button
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_custom_message, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.nameView.text = customMessages[position].name
+        holder.messageView.text = customMessages[position].message
+        holder.overflowButton.setOnClickListener {
+            // TODO: add implementation
+        }
+    }
+
+    override fun getItemCount() = customMessages.size
+}
